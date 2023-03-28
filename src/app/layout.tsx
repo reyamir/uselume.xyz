85674370f805228a022542fbd41a0f9096e1b937
Ren Amamiya <123083837+reyamir@users.noreply.github.com>
2023-03-28 18:12:30 +0700

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,23 @@ import "./globals.css";
 export const metadata = {
   title: "Lume",
   description: "an ambitious nostr client",
+  url: "https://uselume.xyz",
+  siteName: "Lume",
+  images: [
+    {
+      url: "https://void.cat/d/7gFCCa7wMqcLkyX7jpZxL8",
+      width: 800,
+      height: 600,
+    },
+    {
+      url: "https://void.cat/d/FRqmULJHUjrGknsY7eFEHZ",
+      width: 1600,
+      height: 1200,
+      alt: "@2x",
+    },
+  ],
+  locale: "en-US",
+  type: "website",
 };
 
 export default function RootLayout({
